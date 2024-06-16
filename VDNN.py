a4ed16d8c22aab7d40597884bbd9fa0192aa1522
GuarnerosSIA <alejandrowarnsan@gmail.com>
2024-06-16 15:36:16 -0600

    Se tiene un algoritmo de integración tipo Runge-Kutta

--- VDNN.py
+++ VDNN.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from controlClasses.algorithms import ValueDNN
+from controlClasses.functions import DoPri45Step
+from scipy.integrate import RK45,solve_ivp,DOP853
 
 t = 150000
 dt = 0.001
@@ -17,24 +19,24 @@ def sigmoid(x,c):
 
 ADNN = np.zeros((2,2))
 ADNN[:1,1:] = np.eye(1)
-ADNN[1:,:1] = -np.eye(1)*100
-ADNN[1:,1:] = -np.eye(1)*50
+ADNN[1:,:1] = np.eye(1)*2
+ADNN[1:,1:] = -np.eye(1)*1
 
 BDNN = np.zeros((2,1))
 BDNN[1:,:] = np.eye(1)
 
 P0 =  np.zeros((2,2))
-P0[0,0] = 20
+P0[0,0] = 0
 P0[0,1] = 0
 P0[1,0] = 0
-P0[1,1] = 20
+P0[1,1] = 0
 
 
 QDNN = np.zeros((2,2))
-QDNN[:1,:1] = np.eye(1)*1.2
-QDNN[1:,1:] = np.eye(1)*1.2
+QDNN[:1,:1] = np.eye(1)*2
+QDNN[1:,1:] = np.eye(1)*1
 
-RDNN = np.eye(1)*0.001
+RDNN = np.eye(1)*0.5
 
 alpha = 0.9
 beta =  0.9
@@ -46,25 +48,58 @@ dnnV = ValueDNN(QDNN,RDNN,BDNN,ADNN,P0,alpha,beta,dt,w0,c)
 delta = np.random.random((2,1))
 print(dnnV.w0[0][0])
 weights = [dnnV.w0[0][0]]
-for i in range(t-1):
+
+steps = 1000
+t = np.linspace(0,15,steps)
+Pint = []
+time = []
+P0vec = P0.flatten()
+def riccati(t,p):
+    # p = p.reshape((2,2))
+    pa = p@ADNN
+    ap = ADNN.T@p
+    pb = p@BDNN
+    q = QDNN
+    rInv = np.linalg.inv(RDNN)
+    bp = BDNN.T@p
+    return -1*(-pa-ap-q + pb@rInv@bp)
+    
+
+p11 = []
+p21 = []
+p12 = []
+p22 = []
+
+
+for i in range(steps-1):
+    h = t[i+1]-t[i]
+    v4, v5 = DoPri45Step(riccati,t[i],P0,h)
+    newP = P0 + h*v5
+    
+    p11.append(newP[0][0])
+    p21.append(newP[1][0])
+    p12.append(newP[0][1])
+    p22.append(newP[1][1])
+    P0 = newP
+
     # dnnV.pUpdate()
     # print(dnnV.valueFunction(delta))
-    dnnV.wUpdate(delta)
-    delta = delta*0.95
-    weights.append(dnnV.w0[0][0])
+    # dnnV.wUpdate(delta)
+    # delta = delta*0.95
+    # weights.append(dnnV.w0[0][0])
     
 print(dnnV.w0[0][0])
 
-p11 = [x[0,0] for x in dnnV.P]
-p21 = [x[1,0] for x in dnnV.P]
-p12 = [x[0,1] for x in dnnV.P]
-p22 = [x[1,1] for x in dnnV.P]
+# p11 = [x[0,0] for x in dnnV.P]
+# p21 = [x[1,0] for x in dnnV.P]
+# p12 = [x[0,1] for x in dnnV.P]
+# p22 = [x[1,1] for x in dnnV.P]
 
-# plt.plot(p11)
-# plt.plot(p21)
-# plt.plot(p12)
-# plt.plot(p22)
-plt.plot(weights)
+plt.plot(t[:-1],p11)
+plt.plot(t[:-1],p21)
+plt.plot(t[:-1],p12)
+plt.plot(t[:-1],p22)
+
 
 plt.show()
 
--- controlClasses/dormandprinceMethod.py
+++ controlClasses/dormandprinceMethod.py
@@ -2,6 +2,21 @@ from scipy.integrate import RK45,solve_ivp
 import numpy as np
 import matplotlib.pyplot as plt
 
+def DoPri45Step(f,t,x,h):
+    
+    k1 = f(t,x)
+    k2 = f(t + 1./5*h, x + h*(1./5*k1) )
+    k3 = f(t + 3./10*h, x + h*(3./40*k1 + 9./40*k2) )
+    k4 = f(t + 4./5*h, x + h*(44./45*k1 - 56./15*k2 + 32./9*k3) )
+    k5 = f(t + 8./9*h, x + h*(19372./6561*k1 - 25360./2187*k2 + 64448./6561*k3 - 212./729*k4) )
+    k6 = f(t + h, x + h*(9017./3168*k1 - 355./33*k2 + 46732./5247*k3 + 49./176*k4 - 5103./18656*k5) )
+
+    v5 = 35./384*k1 + 500./1113*k3 + 125./192*k4 - 2187./6784*k5 + 11./84*k6
+    k7 = f(t + h, x + h*v5)
+    v4 = 5179./57600*k1 + 7571./16695*k3 + 393./640*k4 - 92097./339200*k5 + 187./2100*k6 + 1./40*k7
+    
+    return v4,v5
+
 def sin_func(t,x):
     return -2*x*delta
 
@@ -12,10 +27,13 @@ def sin_func_args(t,x,delta):
 
 t = np.linspace(0,np.pi,101)
 x0 = [100]
+x00 = 100
 y = []
 yb = []
+yManuak = []
 time = []
 timeb = []
+timeManual = []
 delta=-1
 sol = RK45(sin_func,t[0],x0,t_bound=5,max_step=0.05)
 
@@ -25,6 +43,11 @@ for i in range(100):
     x0 = [sol_args.y[0][-1]]
     yb.append(x0[0])
     timeb.append(sol_args.t[-1])
+    h = t[i+1]-t[i]
+    v4, v5 = DoPri45Step(sin_func,t[i],x00,h)
+    yManuak.append(x00 + h*v5)
+    x00 = yManuak[-1]
+
     # print(x0)
     y.append(sol.y[0])
     time.append(sol.t)
@@ -38,4 +61,5 @@ plt.plot(time,y)
 new = [100*np.exp(-2*x) for x in time]
 plt.plot(time,new)
 plt.plot(timeb, yb)
+plt.plot(t[:-1],yManuak)
 plt.show()
