36990e25c411b4acbbbd5c1bc8dc3cb19fe4033c
GuarnerosSIA <alejandrowarnsan@gmail.com>
2024-07-07 13:52:54 +0100

    New plot

--- integrationExamples.py
+++ integrationExamples.py
@@ -2,69 +2,84 @@ import numpy as np
 import matplotlib.pyplot as plt
 from controlClasses.algorithms import ValueDNN
 from controlClasses.functions import DoPri45Step
+from controlClasses.constants import*
+from matplotlib import rcParams
+rcParams.update({'font.family': 'Times New Roman',
+                 'font.size':10,
+                 'text.usetex':True})
 
-
-ADNN = np.zeros((2,2))
-ADNN[:1,1:] = np.eye(1)
-ADNN[1:,:1] = np.eye(1)*2
-ADNN[1:,1:] = -np.eye(1)*1
-
-BDNN = np.zeros((2,1))
-BDNN[1:,:] = np.eye(1)
-
-P0 =  np.zeros((2,2))
-P0[0,0] = 0
-P0[0,1] = 0
-P0[1,0] = 0
-P0[1,1] = 0
-
-
-QDNN = np.zeros((2,2))
-QDNN[:1,:1] = np.eye(1)*2
-QDNN[1:,1:] = np.eye(1)*1
-
-RDNN = np.eye(1)*0.5
-
-steps = 1000
-t = np.linspace(0,15,steps)
-Pint = []
+steps = 4000
+t = np.linspace(0,40,steps)
+Pint = np.zeros((steps,144))
 time = []
 
 def riccati(t,p):
     # p = p.reshape((2,2))
-    pa = p@ADNN
-    ap = ADNN.T@p
-    pb = p@BDNN
-    q = QDNN
-    rInv = np.linalg.inv(RDNN)
-    bp = BDNN.T@p
+    pa = p@ALQR3
+    ap = ALQR3.T@p
+    pb = p@BLQR3
+    q = QLQR3
+    rInv = np.linalg.inv(RLQR3)
+    bp = BLQR3.T@p
     return -1*(-pa-ap-q + pb@rInv@bp)
     
 
-p11 = []
-p21 = []
-p12 = []
-p22 = []
-
-
+Pint[0,:] = PLQR3.flatten()
 for i in range(steps-1):
     h = t[i+1]-t[i]
-    v4, v5 = DoPri45Step(riccati,t[i],P0,h)
-    newP = P0 + h*v5
+    v4, v5 = DoPri45Step(riccati,t[i],PLQR3,h)
+    newP = PLQR3 + h*v5
     
-    p11.append(newP[0][0])
-    p21.append(newP[1][0])
-    p12.append(newP[0][1])
-    p22.append(newP[1][1])
-    P0 = newP
-
-plt.title('Riccati Differential equation')
-plt.ylabel('P dynamics')
-plt.xlabel('Time (s)')
-plt.plot(t[:-1],p11)
-plt.plot(t[:-1],p21)
-plt.plot(t[:-1],p12)
-plt.plot(t[:-1],p22)
+    
+    # p11.append(newP[0][0])
+    # p21.append(newP[1][0])
+    # p12.append(newP[0][1])
+    # p22.append(newP[1][1])
+    PLQR3 = newP
+    Pint[i+1,:] = PLQR3.flatten()
 
 
-plt.show()
+print(PLQR3)
+
+plt.figure(figsize=(4,4),dpi=300)
+
+plt.axes([0.15, 0.15, 0.8, 0.8])
+
+ax = plt.gca()
+
+
+ax.plot(t,Pint[:,0], label = 'LQR+PD', 
+        linewidth=5,linestyle='-',color='steelblue')
+ax.plot(t,Pint[:,6], label = 'LQR+PD', 
+        linewidth=5,linestyle='-',color='tab:red')
+ax.plot(t,Pint[:,78], label = 'LQR+PD', 
+        linewidth=5,linestyle='-',color='seagreen')
+# ax.plot(time, dnnControl['LQR Integral value function'], label = 'LQR+DNN',
+#          linewidth=5,linestyle='-', color = 'tab:red')
+# ax.set_xlim(36,40)
+# ax.set_ylim(1.45e7,1.7e7)
+ax.set_ylabel(r'Values from $P_{t}$',size=12)
+ax.set_xlabel(r'Time (s)',size=12)
+ax.grid(True)
+# ax.legend(fontsize=10)
+
+plt.axes([0.35, 0.35, 0.5, 0.55])
+
+ax = plt.gca()
+
+
+ax.plot(t,Pint[:,0], label = r'$P_{i,i}$', 
+        linewidth=5,linestyle='-',color='steelblue')
+ax.plot(t,Pint[:,6], label = r'$P_{i,j}=P_{j,i}$', 
+        linewidth=5,linestyle='-',color='tab:red')
+ax.plot(t,Pint[:,78], label = r'$P_{j,j}$', 
+        linewidth=5,linestyle='-',color='seagreen')
+# ax.plot(time, dnnControl['LQR Integral value function'], label = 'LQR+DNN',
+#          linewidth=5,linestyle='-', color = 'tab:red')
+ax.set_xlim(0,1)
+# ax.set_ylim(1.45e7,1.7e7)
+ax.grid(True)
+ax.legend(fontsize=10)
+
+
+plt.savefig('./Riccati.png')
--- plotingResults.py
+++ plotingResults.py
@@ -17,59 +17,103 @@ rcParams.update({'font.family': 'Times New Roman',
 
 
 for i in range(6):
-    fig,ax = plt.subplots(2,1, dpi=300)
-
-    fig.set_figheight(3)
-    fig.set_figwidth(7)
-
     system = 'System '+str(i+1)
     reference = 'Reference '+str(i+1)
-    control = 'Control '+str(i+1)
+    file = './Tracking'+str(i+1)+'.png'
 
-    file = './Actuator'+str(i+1)+'.png'
+    plt.figure(figsize=(4,4),dpi=300)
 
-    ax[0].plot(tiempo,lqrControl[reference], label = 'Reference',linewidth=10, color = 'black')
-    ax[0].plot(tiempo, dnnControl[system], linewidth=7, color = 'tab:red')
-    ax[0].plot(tiempo,lqrControl[system], linewidth=4, color = 'steelblue')
+    plt.axes([0.15, 0.15, 0.8, 0.8])
+
+    ax = plt.gca()
+    ax.plot(tiempo,lqrControl[reference],linewidth=10, color = 'black')
+    ax.plot(tiempo, dnnControl[system], linewidth=7, color = 'tab:red')
+    ax.plot(tiempo,lqrControl[system], linewidth=4, color = 'steelblue')
     
-    ax[0].set_ylabel(r'Distance (cm)',size=12)
-    ax[0].set_xlim(0,40)
-    ax[0].set_ylim(2.5,8)
-    ax[0].grid(True)
-    ax[0].legend(fontsize=10)
+    ax.set_ylabel(r'Distance (cm)',size=12)
+    ax.set_xlabel(r'Time (s)',size=12)
+    ax.set_xlim(0,40)
+    ax.set_ylim(2.5,15)
+    ax.grid(True)
+    plt.axes([0.3, 0.55, 0.6, 0.35])
 
-    ax[1].plot(tiempo,dnnControl[control], label = 'LQR+DNN', linewidth=2, color = 'tab:red')
-    ax[1].plot(tiempo,lqrControl[control], label = 'LQR+PD', linewidth=2, color = 'steelblue')
+    ax = plt.gca()
+    
+    ax.plot(tiempo,lqrControl[reference], label = 'Reference',linewidth=3, color = 'black')
+    ax.plot(tiempo, dnnControl[system], label = 'LQR+DNN', linewidth=3, color = 'tab:red')
+    ax.plot(tiempo,lqrControl[system], label = 'LQR+PD', linewidth=3, color = 'steelblue')
+    
+    ax.set_xlim(24,28)
+    ax.set_ylim(6,7.1)
+    ax.grid(True)
+    ax.legend(fontsize=10)
 
-    ax[1].set_ylabel(r'PWM',size=12)
-    ax[1].set_xlim(0,40)
-    ax[1].set_ylim(-200,350)
-    ax[1].grid(True)
-    ax[1].legend()
-
-    ax[1].set_xlabel(r'Time (s)',size=12)
-
-    plt.tight_layout()
-    fig.savefig(file)
+    # plt.tight_layout()
+    plt.savefig(file)
 
 
-fig,ax = plt.subplots(1,1)
+for i in range(6):
+    control = 'Control '+str(i+1)
+    file = './Control'+str(i+1)+'.png'
 
-fig.set_figheight(3)
-fig.set_figwidth(7)
+    plt.figure(figsize=(4,4),dpi=300)
+
+    plt.axes([0.2, 0.15, 0.75, 0.8])
+
+    ax = plt.gca()
+    ax.plot(tiempo,dnnControl[control], label = 'LQR+DNN', linewidth=2, color = 'tab:red')
+    ax.plot(tiempo,lqrControl[control], label = 'LQR+PD', linewidth=2, color = 'steelblue')
+
+    ax.set_ylabel(r'PWM',size=12)
+    ax.set_xlim(0,40)
+    ax.set_ylim(-150,500)
+    ax.grid(True)
+    ax.set_xlabel(r'Time (s)',size=12)
+
+
+    plt.axes([0.35, 0.55, 0.5, 0.35])
+
+    ax = plt.gca()
+    ax.plot(tiempo,dnnControl[control], label = 'LQR+DNN', linewidth=2, color = 'tab:red')
+    ax.plot(tiempo,lqrControl[control], label = 'LQR+PD', linewidth=2, color = 'steelblue')
+
+    ax.set_xlim(24,28)
+    ax.set_ylim(-150,100)
+    ax.grid(True)
+    ax.legend()
+
+    plt.savefig(file)
+
+
+
+plt.figure(figsize=(4,4),dpi=300)
+
+plt.axes([0.2, 0.15, 0.75, 0.8])
+
+ax = plt.gca()
 
 ax.plot(tiempo,lqrControl['LQR Integral value function'], label = 'LQR+PD', 
         linewidth=5,linestyle='-', color = 'steelblue')
 ax.plot(tiempo, dnnControl['LQR Integral value function'], label = 'LQR+DNN',
-         linewidth=5,linestyle='-', color = 'tab:red',alpha=0.7)
-ax.set_ylabel(r'Functional $J_{t}$',size=12)
+         linewidth=5,linestyle='-', color = 'tab:red')
+ax.set_ylabel(r'$J_{t}(t_{0},\Delta_{0};u_{op})$',size=12)
+ax.set_xlabel(r'Time (s)')
 ax.set_xlim(0,40)
-# ax.set_ylim(2.5,8)
+ax.set_ylim(0,3.5e7)
+ax.grid(True)
+
+plt.axes([0.35, 0.55, 0.5, 0.35])
+
+ax = plt.gca()
+
+ax.plot(tiempo,lqrControl['LQR Integral value function'], label = 'LQR+PD', 
+        linewidth=5,linestyle='-', color = 'steelblue')
+ax.plot(tiempo, dnnControl['LQR Integral value function'], label = 'LQR+DNN',
+         linewidth=5,linestyle='-', color = 'tab:red')
+ax.set_xlim(36,40)
+ax.set_ylim(1.45e7,1.7e7)
 ax.grid(True)
 ax.legend(fontsize=10)
 
 
-plt.tight_layout()
-
-
-fig.savefig('./functional.png')
+plt.savefig('./functional.png')
